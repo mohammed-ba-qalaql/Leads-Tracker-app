cd060bd85b97a732e9c85e28348ef3897c214bf7
baqlaqel <mohammedbaqlaqel@gmail.com>
2025-08-10 23:29:05 +0300

    delete everything related to localStorage aand myLeads array to setup the firebase realtime database

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 const firebaseConfig = {
     databaseURL : "https://leads-tracker-app-49ace-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -10,28 +10,16 @@ const database = getDatabase(app)
 
 let inputEl = document.getElementById("text-el")
 
-let myLeads = [];
-
 let ulEl = document.getElementById("ul-el")
 
-let localLeads = JSON.parse(localStorage.getItem("myLeads"))
-if(localLeads){
-    myLeads = localLeads
-    render(myLeads)
-}
 
 let saveBtn = document.getElementById("save-btn")
 saveBtn.addEventListener("click" , function(){
-    myLeads.push(inputEl.value);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
     inputEl.value = ""
 })
 
 const deleteBtn = document.getElementById("delete-btn")
 deleteBtn.addEventListener("dblclick",function(){
-    localStorage.clear()
-    myLeads = []
     ulEl.innerHTML = ""
 })
 
