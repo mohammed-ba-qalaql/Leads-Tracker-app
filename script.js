4c66191f37bdc925d02e7e3f8f61739fa3c32d1e
baqlaqel <mohammedbaqlaqel@gmail.com>
2025-08-11 15:13:49 +0300

    finish the app with proper conversion of storage used and some adjustments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 const firebaseConfig = {
     databaseURL : "https://leads-tracker-app-49ace-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -14,17 +14,24 @@ let inputEl = document.getElementById("text-el")
 let ulEl = document.getElementById("ul-el")
 
 onValue(referenceInDB, function(snapshot){
-    console.log(snapshot.val())
+    const snapshotExist = snapshot.exists()
+    if(snapshotExist){
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+    
 })
 
 let saveBtn = document.getElementById("save-btn")
 saveBtn.addEventListener("click" , function(){
-    push(referenceInDB, inputEl.value)
+    push(referenceInDB, `https://${inputEl.value}`)
     inputEl.value = ""
 })
 
 const deleteBtn = document.getElementById("delete-btn")
 deleteBtn.addEventListener("dblclick",function(){
+    remove(referenceInDB)
     ulEl.innerHTML = ""
 })
 
